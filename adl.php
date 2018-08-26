d8c7a5e25eb4ba7babce4c737b7f70a77ea82eb3
pidewap <pidewap@gmail.com>
2018-08-26 22:02:38 +0700

    Update adl.php

--- adl.php
+++ adl.php
@@ -2,12 +2,22 @@
 $json_url = 'https://www.saveoffline.com/process/?url=https://www.youtube.com/watch?v='.$_GET['id'].'&type=json';
 $json = file_get_contents($json_url);
 $data = json_decode($json, TRUE);
-echo '<pre>';
-print_r($data);
-echo '720p - mp4 = '.$data[urls][0][id].'';
-echo '360p - mp4 = '.$data[urls][1][id].'';
-echo '360p - webm = '.$data[urls][2][id].'';
-echo '240p - 3gp = '.$data[urls][3][id].'';
-echo '144p - 3gp = '.$data[urls][4][id].'';
-echo '</pre>';
+
+if($_GET['type'] == '720'){
+  $url = $data[urls][0][id];
+}
+if($_GET['type'] == '360p'){
+  $url = $data[urls][1][id];
+}
+if($_GET['type'] == '360w'){
+  $url = $data[urls][2][id];
+}
+if($_GET['type'] == '240p'){
+  $url = $data[urls][3][id];
+}
+if($_GET['type'] == '144p'){
+  $url = $data[urls][4][id];
+}
+header(Location: '.$url.');
+exit();
 ?>
