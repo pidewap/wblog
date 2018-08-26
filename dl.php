2547335b1f5d3f95cef41c0cfb78eed4245e9975
pidewap <pidewap@gmail.com>
2018-08-26 21:42:27 +0700

    Update and rename dl.php to index.php

--- dl.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$json_url = 'https://www.saveoffline.com/process/?url=https://www.youtube.com/watch?v=msDPN34kHiQ&type=json';
-$json = file_get_contents($json_url);
-$data = json_decode($json, TRUE);
-echo '<pre>';
-print_r($data);
-echo '</pre>';
-?>
