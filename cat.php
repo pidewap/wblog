cefe83d1cb16684a52062be5dab39900432a57b7
pidewap <pidewap@gmail.com>
2018-08-27 10:21:02 +0700

    Update cat.php

--- cat.php
+++ cat.php
@@ -79,7 +79,7 @@ echo '
 {{ block( 'search' ) }}
   
   &lt;div class="wrapper"&gt;&lt;div class="menu-home"&gt;&lt;h2 class="title-menu"&gt;<?php echo str_replace(' - BeLagu','',$ltitle); ?> - 100 Music&lt;/h2&gt;
-&lt;div class="notifin"&gt;Latest update on: <?php echo date("l, Y-m-d / h:i"); ?>&lt;/div&gt;
+&lt;div class="notifin"&gt;Latest update on: <?php echo date("l, Y-m-d"); ?>&lt;/div&gt;
   
   {% if top|length &gt; 0 %}
 {% for video in top|slice(0, 100) %}
