06dfcefa779b478296fa2d9bbe79490ce86611f1
pidewap <pidewap@gmail.com>
2018-08-27 10:39:35 +0700

    Update wdownload.php

--- wdownload.php
+++ wdownload.php
@@ -36,9 +36,9 @@ echo '
 
 
     {% if data %}
-  &lt;div class="wrapper"&gt;&lt;div class="menu-home"&gt;&lt;h2 class="title-menu"&gt;Download Part&lt;/h2&gt;
+  &lt;div class="wrapper"&gt;&lt;div class="menu-home"&gt;&lt;h2 class="title-menu"&gt;Download Section&lt;/h2&gt;
 &lt;div class="notifin"&gt;
-Download &lt;strong&gt;{{ data.fulltitle|raw }}&lt;/strong&gt; as video and song. This content is free of copyright, &lt;b&gt;BeLagu&lt;/b&gt; is a free search engine.
+Download &lt;strong&gt;{{ data.fulltitle|raw }}&lt;/strong&gt; as video and song. This content is free of copyright, &lt;b&gt;WBlog&lt;/b&gt; is a free search engine.
 &lt;/div&gt;
   &lt;div class="menulist"&gt;
 &lt;div style="border-top:0px dashed #ddd;margin:20px 0px 5px;padding:5px 3px;"&gt;&lt;div id="play" align="center"&gt;
