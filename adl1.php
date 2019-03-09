28a981fa7545761a2a9e53f8494a6538d1a041d3
pidewap <pidewap@gmail.com>
2019-03-10 05:23:02 +0800

    Create adl1.php

---
+++ adl1.php
@@ -0,0 +1,29 @@
+<?php 
+    $url = 'https://www.saveoffline.com/process/?url=https://www.youtube.com/watch?v='.$_GET['id'].'&type=json';
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec($curl);
+
+$jsob=json_decode($data);
+foreach ($jsob->urls as $item)
+{
+    if($_GET['type'] == 'mp4'){
+if($item->label == '360p - mp4')
+    {
+  $urld = $item->id;
+    }
+}
+
+if($_GET['type'] == 'm4a'){
+if($item->label == '(audio - no video) m4a (128 kbps)')
+{
+$urld = $item->id;
+}
+}
+}
+
+
+header ("Location: " . $urld. "");
+?>
+ 
