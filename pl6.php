1b5c4758bc6ac9bd43c988eb6b257ea29c426fa8
pidewap <pidewap@gmail.com>
2020-02-25 09:03:46 +0800

    Update pl6.php

--- pl6.php
+++ pl6.php
@@ -23,7 +23,7 @@ echo ''.$n.': {url:"'.clean($artist).'-'.clean($title).'", title: "'.str_replace
 }
 echo '
 } %}<br><br>';
-print_r($top_albums);
+print_r($top_albums->feed->results);
 ?>
 {% block toplagu %}
 {% endblock %}
