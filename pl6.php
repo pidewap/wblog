07222b96c0d79f16dae029002a5a823d4d9608b4
pidewap <pidewap@gmail.com>
2020-02-25 10:42:09 +0800

    Update pl6.php

--- pl6.php
+++ pl6.php
@@ -4,10 +4,10 @@
 $string = str_replace('&', '', $string);
    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
 }
-$content=file_get_contents('https://rss.itunes.apple.com/api/v1/id/apple-music/new-releases/all/10/explicit.json'); 
+$content=file_get_contents('https://rss.itunes.apple.com/api/v1/id/apple-music/new-releases/all/100/explicit.json'); 
 $top_albums=json_decode($content);
 $tracks = $top_albums->feed->results;
-echo '{% set top = {
+echo '{% set indob = {
 ';
 foreach( $tracks as $track ) {
 $img = $track->artworkUrl100;
@@ -25,4 +25,29 @@ echo '
 } %}';
 ?>
 {% block indobaru %}
+{% endblock %}
+
+<?php
+
+$tcontent=file_get_contents('https://rss.itunes.apple.com/api/v1/id/itunes-music/top-songs/all/100/explicit.json'); 
+$ttop_albums=json_decode($tcontent);
+$ttracks = $ttop_albums->feed->results;
+echo '{% set indot = {
+';
+foreach( $ttracks as $ttrack ) {
+$img = $track->artworkUrl100;
+  $ttitle = $ttrack->name;
+  $tartist = $ttrack->artistName;
+  $tdate = $ttrack->releaseDate;
+  $tadate=date('j F Y', strtotime($tdate));
+  $tcat = $ttrack->genres[0]->name;
+ 
+$n=rand(0,100000);
+echo ''.$n.': {url:"'.clean($tartist).'-'.clean($ttitle).'", title: "'.str_replace(',','',str_replace('"','',$ttitle)).'", artist: "'.str_replace(',','',str_replace('"','',$tartist)).'", img: "'.$timg.'", date: "'.$tadate.'", cat: "'.$tcat.'"},
+';
+}
+echo '
+} %}';
+?>
+{% block indotop %}
 {% endblock %}
