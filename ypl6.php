2b4374e4be3e4196866a6c6d172db9fc43bc499c
pidewap <pidewap@gmail.com>
2020-06-27 09:27:05 +0800

    Create ypl6.php

---
+++ ypl6.php
@@ -0,0 +1,18 @@
+<?php
+$content=file_get_contents('https://www.youtube.com'); 
+$top_albums=json_decode($content);
+$tracks = $top_albums->feed->entry;
+echo '{% set nav = {
+';
+foreach( $tracks as $track ) {
+$img = $track->{'im:image'}[0]->label;
+  $title = $track->{'im:name'}->label;
+  $artist = $track->{'im:artist'}->label;
+$n=rand(0,100000);
+echo ''.$n.': {title: "'.str_replace(',','',str_replace('"','',$title)).'", artist: "'.str_replace(',','',str_replace('"','',$artist)).'", img: "'.$img.'"},
+';
+}
+echo '
+} %}';
+echo $content;
+?>
