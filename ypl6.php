8f3b2489562b09b3889b6c113c5d5b9f72c79c13
pidewap <pidewap@gmail.com>
2020-07-02 11:00:54 +0800

    Update ypl6.php

--- ypl6.php
+++ ypl6.php
@@ -14,63 +14,24 @@ $uar=array('Nokia2610/2.0 (07.04a) Profile/MIDP-2.0 Configuration/CLDC-1.1 UP.Li
 $uarr=array_rand($uar);
 $uarand=$uar[$uarr];
 
-
-if(!empty($_GET['page'])){
-$pages='page/'.$_GET['page'].'/';
-}else{
-  $pages='';
-}
-
 if(!empty($_GET['url'])){
   $urr=$_GET['url'];
 }else{
-  $urr='https://k2nblog.com/category/single-album/'.$pages.'';
+  $urr='https://go-lagu.com/cari?q=bts';
 }
 
 
 $f=file(''.$urr.'');
 $gg=@implode($f);
-$bod=maling($gg, '<body data', '</html>');
-$bod=str_replace('/"', '"', $bod);
-$bod=str_replace('https://k2nblog.com/category/single-album/k-pop/page/', '/ppo.php?page=', $bod);
-$bod=str_replace('https://k2nblog.com/category/single-album/page/', '/ppo.php?page=', $bod);
-$bod=str_replace('https://k2nblog.com/', '/ppo.php?url=https://k2nblog.com/', $bod);
-   $artist=maling($bod, 'description" content="',' - ');
-if(!empty($_GET['url'])){
-  $tite=maling($bod, '<h1 class="entry-title">', '</h1>');
-  $artist=maling($gg, 'property="og:description" content="', ' - ');
-  $imgs=maling($gg, '<p><center><img src="', '"');
-  $content=maling($bod, '<div class="td-post-content">', '<p>Track');
-  $rd=maling($content, 'Date: ', '<');
-  $genre=maling($content, 'Genre: ', '<');
-  $lang=maling($content, 'Language: ', '<');
-  $br=maling($content, 'Bit Rate: ', '<');
-  echo '<form action="http://satriamusic.com/admin" method="post" name="form"><div class="menu">
-  <span class="red">*</span> Title:<br/><input type="text" name="title" id="title"  value="'.$tite.'" required=""/><br/>
-		Thumbnail:<br/><input type="text" name="thumb" id="url" value="'.str_replace('https', 'http', $imgs).'" /><br/>
-      genre:<br/><input type="text" name="genre" id="genre"  value="'.$genre.'" required=""/><br/>
-      bitrate:<br/><input type="text" name="bitrate" id="bitrate"  value="'.$br.'" required=""/><br/>
-      lang:<br/><input type="text" name="lang" id="lang"  value="'.$lang.'" required=""/><br/>
-      date:<br/><input type="text" name="date" id="date"  value="'.$rd.'" required=""/><br/>
-      <span class="red">*</span> Content: <br/><textarea rows="10" name="noidung" id="msg">';
-   $hasil=explode('<p>Track List:',$bod);
-$hasil=explode('</p>',$hasil[1]);
-$hasil=explode('<br />',$hasil[0]);
-  $hasil = str_replace('&#038;', '&', $hasil);
-  $hasil = preg_replace("/\d+/u","<new>",$hasil);
+$bod=maling($gg, '<body', '</html>');
+$hasil=explode('<div class="go-archive media">',$bod);
+$hasil=explode('</div></div> ',$hasil[1]);
+$hasil=explode('<a href="',$hasil[0]);
    for($i=1;$i<count($hasil);$i++){
-      $link=explode('<new>. ',$hasil[$i]);
-$link=explode('<br',$link[1]);
+      $link=explode('https://download.go-lagu.com/download-lagu-',$hasil[$i]);
+$link=explode('/',$link[1]);
 $link=$link[0];
-     echo ''.$i.'. <strong>'.$artist.' - '.$link.'</strong> [<a href="/search/'.strtolower(urlencode($artist)).'+-+'.strtolower(urlencode($link)).'" target="_blank">Download</a>]<br>';
+     echo ''.$i.'. '.$link.'';
       }
-  echo '</textarea><br/>
-		Keywords: separated by commas <span class="red">, </span><br/>
-    <input type="text" name="key" value="" required=""/><br/>
-    <span class="red">*</span> Categories:<br/>
-    <select name="idcm" style="max-width:50%;"><option value="">Select category</option><optgroup label="-- Kpop --"><option value="soundtrack">Soundtrack</option><option value="single">Single</option><option value="album">Album</option></optgroup></select><br/></div>
-    <div class="menu"><input name="" type="submit" value="Post"></div></form>';
-}else{
-echo strip_tags($bod, '<a><div><p><br>');
-}
+  
 ?>
